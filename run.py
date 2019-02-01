5db1831eaef895bf07a99ea07d7f57f25346f910
Wojtek Paturaj <wojtek.paturaj@gmail.com>
2019-02-01 01:49:47 +0100

    ddq

--- run.py
+++ run.py
@@ -1,6 +1,8 @@
 from app import create_app
 from sqlalchemy.orm import scoped_session, sessionmaker, Query
-
+import os
 # if __name__=='__main__':
 flask_app=create_app('config')
 flask_app.run()
+port = int(os.environ.get("PORT", 5000))
+flask_app.run(host='0.0.0.0', port=port)
